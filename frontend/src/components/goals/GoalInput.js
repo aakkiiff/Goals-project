7429f7dcefcddf4edac012c3ba9cf023ac5eb8d9
akiffo <jackakif@gmail.com>
2022-06-06 16:17:35 +0600

    cicd test

--- frontend/src/components/goals/GoalInput.js
+++ frontend/src/components/goals/GoalInput.js
@@ -34,7 +34,7 @@ function GoalInput(props) {
             value={enteredGoalText}
             onChange={updateGoalTextHandler}
           />
-          <button>Add Goal</button>
+          <button>Add Goal v2</button>
         </form>
       </Card>
     </section>
