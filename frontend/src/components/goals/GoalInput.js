d914d9799fca6e327153f1fcf2af36908b11fc34
akif <104318101+aakkiiff@users.noreply.github.com>
2024-04-06 16:15:36 +0600

    Update GoalInput.js

--- frontend/src/components/goals/GoalInput.js
+++ frontend/src/components/goals/GoalInput.js
@@ -34,7 +34,7 @@ function GoalInput(props) {
             value={enteredGoalText}
             onChange={updateGoalTextHandler}
           />
-          <button>Add Goal v2</button>
+          <button>Add Goal</button>
         </form>
       </Card>
     </section>
