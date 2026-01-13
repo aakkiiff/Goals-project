340f6692b61fc64e67e02f4e4be293359d21cead
aakkiiff <akif22inbox@gmail.com>
2026-01-14 00:20:08 +0600

    phase 4-demo changes updated

--- backend/app.js
+++ backend/app.js
@@ -84,7 +84,7 @@ app.delete('/goals/:id', async (req, res) => {
 });
 
 mongoose.connect(
-  `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_URL}`,
+  `mongodb://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_URL}`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
